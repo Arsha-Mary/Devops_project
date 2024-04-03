705b2316d1efc7491f3e73f44fec695675e88f82
Arsha-Mary <122686375+Arsha-Mary@users.noreply.github.com>
2024-04-03 15:27:58 -0400

    Add files via upload

---
+++ database.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+       host : 'db4free.net',
+       database : 'devopsproject_1',
+       user : 'devops_proj_1',
+       password : 'Devops_project_1',
+       connectTimeout: 20000
+
+
+});
+
+connection.connect(function(error){
+    if(error)
+    {
+        throw error;
+    }
+    else
+    {
+        console.log('MySQL Database is connected Successfully');
+    }
+
+
+
+
+});
+
+module.exports = connection;
